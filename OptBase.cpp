e6835746bef2cfede9e65efda07dc59173aeb6d2
Martin Buck <buckmartin@buckmartin.de>
2015-03-25 14:46:05 +0100

    added queues for OptValues

--- OptBase.cpp
+++ OptBase.cpp
@@ -1,5 +1,15 @@
 #include "OptBase.h"
 
+std::mutex
+    OptBase::mutexQueueTodo,
+    OptBase::mutexQueueCalculated,
+    OptBase::mutexQueueFinished;
+
+std::queue<OptValue>
+    OptBase::queueTodo,
+    OptBase::queueCalculated,
+    OptBase::queueFinished;
+
 OptBase::OptBase(const std::vector<OptValue> &optValues)
 {
 
@@ -8,5 +18,53 @@ OptBase::OptBase(const std::vector<OptValue> &optValues)
 OptBase::~OptBase()
 {
 
+}
+
+void OptBase::push_todo(const OptValue &optValue)
+{
+    mutexQueueTodo.lock();
+    queueTodo.push(optValue);
+    mutexQueueTodo.unlock();
+}
+
+void OptBase::push_calculated(const OptValue &optValue)
+{
+    mutexQueueCalculated.lock();
+    queueCalculated.push(optValue);
+    mutexQueueCalculated.unlock();
+}
+
+void OptBase::push_finished(const OptValue &optValue)
+{
+    mutexQueueFinished.lock();
+    queueFinished.push(optValue);
+    mutexQueueFinished.unlock();
+}
+
+OptValue OptBase::pop_todo()
+{
+    mutexQueueTodo.lock();
+    auto out = queueTodo.front();
+    queueTodo.pop();
+    mutexQueueTodo.unlock();
+    return out;
+}
+
+OptValue OptBase::pop_calculated()
+{
+    mutexQueueCalculated.lock();
+    auto out = queueCalculated.front();
+    queueCalculated.pop();
+    mutexQueueCalculated.unlock();
+    return out;
+}
+
+OptValue OptBase::pop_finished()
+{
+    mutexQueueFinished.lock();
+    auto out = queueFinished.front();
+    queueFinished.pop();
+    mutexQueueFinished.unlock();
+    return out;
 }
 
--- OptBase.h
+++ OptBase.h
@@ -2,6 +2,8 @@
 #define OPTBASE_H
 
 #include <vector>
+#include <mutex>
+#include <queue>
 
 #include "defines.h"
 #include "OptValue.h"
@@ -9,9 +11,28 @@
 
 class OptBase
 {
+private:
+    static std::mutex
+        mutexQueueTodo,
+        mutexQueueCalculated,
+        mutexQueueFinished;
+
+    static std::queue<OptValue>
+        queueTodo,
+        queueCalculated,
+        queueFinished;
+
 public:
     OptBase(const std::vector<OptValue> &optValues);
     ~OptBase();
+
+    static void push_todo(const OptValue &optValue);
+    static void push_calculated(const OptValue &optValue);
+    static void push_finished(const OptValue &optValue);
+
+    static OptValue pop_todo();
+    static OptValue pop_calculated();
+    static OptValue pop_finished();
 };
 
 #endif // OPTBASE_H
