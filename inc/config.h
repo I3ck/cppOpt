a20489df6b60fa119baa91e6e545e072e210cae6
Martin Buck <buckmartin@buckmartin.de>
2015-03-31 22:49:40 +0200

    commented config file

--- inc/config.h
+++ inc/config.h
@@ -1,7 +1,12 @@
 #ifndef DEFINES
 #define DEFINES
 
+//comment out or dont
 #define DEBUG
+
+//activate either one of those
+//#define T float
 #define T double
+//#define T long double
 
 #endif // DEFINES
