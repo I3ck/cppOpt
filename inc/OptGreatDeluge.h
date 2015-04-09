5829e0e82e2e82f6a987bb0ab44de14bf1b9ff5b
Martin Buck <buckmartin@buckmartin.de>
2015-04-09 13:45:19 +0200

    GreatDeluge now templated

--- inc/OptGreatDeluge.h
+++ inc/OptGreatDeluge.h
@@ -19,6 +19,7 @@
 namespace cppOpt
 {
 
+template <typename T>
 class OptGreatDeluge : public OptBase
 {
 private:
@@ -34,7 +35,12 @@ private:
         temperature,
         waterLevel;
 
+//------------------------------------------------------------------------------
+
 public:
+
+//------------------------------------------------------------------------------
+
     OptGreatDeluge(const OptBoundaries &optBoundaries,
                           unsigned int maxCalculations,
                           OptSolverBase* pCalculator,
@@ -42,17 +48,132 @@ public:
                           OPT_T targetValue, ///@todo move defaulted ones to the end? or Base => Child like it is currently?
                           OPT_T coolingFactor,
                           OPT_T waterLevel,
-                          OPT_T rain);
+                          OPT_T rain) : 
+        OptBase(optBoundaries, maxCalculations, pCalculator, optTarget, targetValue),
+        coolingFactor(coolingFactor),
+        rain(rain),
+        temperature(1.0),
+        waterLevel(waterLevel)
+    {
 
-    ~OptGreatDeluge();
+    }
+
+//------------------------------------------------------------------------------
+
+    ~OptGreatDeluge()
+    {
+
+    }
+
+//------------------------------------------------------------------------------
 
 private:
-    OptCalculation get_next_calculation();
-    OptCalculation random_start_value();
-    void update_temperature();
-    void update_water_level();
 
-    OptCalculation compare_value() const; ///@todo rename & use T as return value? (=> would require a new compare method)
+//------------------------------------------------------------------------------
+
+    OptCalculation get_next_calculation()
+    {
+        if(previousCalculations.empty())
+            return random_start_value();
+
+        ///@todo merge these definitions
+        OptCalculation newValue;
+        OptCalculation referenceValue;
+
+        OptCalculation compareValue = compare_value();
+
+        if(result_better(previousCalculations.back(), compareValue, optTarget, targetValue))
+            referenceValue = previousCalculations.back();
+
+        else
+            referenceValue = bestCalculation;
+
+        while(true)
+        {
+            newValue = OptCalculation();
+            for(auto boundary = optBoundaries.cbegin(); boundary != optBoundaries.cend(); ++boundary)
+            {
+                ///@todo change logic could be a method
+                OPT_T change, maxChange;
+
+                maxChange = 0.5 * boundary->range() * temperature;
+                change = random_factor() * maxChange;
+
+                if(rand() % 2)
+                    change *= -1.0;
+
+                newValue.add_parameter(boundary->name, referenceValue.get_parameter(boundary->name) + change);
+            }
+            if(valid(newValue))
+                break;
+        }
+
+        update_temperature();
+        update_water_level();
+        return newValue;
+    }
+
+//------------------------------------------------------------------------------
+
+    OptCalculation random_start_value()
+    {
+        OptCalculation optCalculation = random_calculation();
+        bestCalculation = optCalculation;
+        bestCalculation.result = bad_value(); ///@todo bestCalculation logic should be moved to general OptBase (since it's gonna repeat itself)
+        return optCalculation;
+    }
+
+//------------------------------------------------------------------------------
+
+    void update_temperature()
+    {
+        temperature *= coolingFactor;
+    }
+
+//------------------------------------------------------------------------------
+
+    void update_water_level()
+    {
+        switch(optTarget)
+        {
+            case MINIMIZE:
+                waterLevel -= rain;
+                break;
+
+            case MAXIMIZE:
+                waterLevel += rain;
+                break;
+
+            case APPROACH:
+                if(targetValue > waterLevel)
+                    waterLevel += rain;
+                else
+                    waterLevel -= rain;
+                break;
+
+            case DIVERGE:
+                if(targetValue > waterLevel)
+                    waterLevel -= rain;
+                else
+                    waterLevel += rain;
+                break;
+
+            default: // MINIMIZE
+                waterLevel -= rain;
+        }
+    }
+
+//------------------------------------------------------------------------------
+
+    OptCalculation compare_value() const ///@todo rename & use T as return value? (=> would require a new compare method)
+    {
+        OptCalculation out;
+        out.result = waterLevel;
+        return out;
+    }
+
+//------------------------------------------------------------------------------
+
 };
 
 #endif // OPTGREATDELUGE_H
