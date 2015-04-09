0e892223aeb054d2044259f1d5fd2b8b72f890a1
Martin Buck <buckmartin@buckmartin.de>
2015-04-09 16:44:52 +0200

    removed the tpe definitions from the config.h

--- inc/config.h
+++ inc/config.h
@@ -15,11 +15,6 @@
 #define CONFIG_H
 
 //comment out or dont
-#define DEBUG
-
-//activate either one of those
-//#define OPT_T float
-#define OPT_T double
-//#define OPT_T long double
+//#define DEBUG
 
 #endif // CONFIG_H
