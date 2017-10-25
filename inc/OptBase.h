137759534ea633608a613e75f9a41e6e11905be3
Martin Buck <buckmartin@buckmartin.de>
2017-10-25 20:08:22 +0200

    further work on splitting controller and algorithm, still not compiling

--- inc/OptBase.h
+++ inc/OptBase.h
@@ -109,6 +109,393 @@ protected:
     }
 };
 
+using namespace std;
+
+template <typename T>
+using calc_t = function<void(OptCalculation<T>&)>; ///@todo own file?
+
+using lock = lock_guard<recursive_mutex>;
+
+template <typename T, bool isMultiThreaded = true>
+class OptCoordinator
+{
+    using self = OptCoordinator<T, isMultiThreaded>;
+
+private:
+    recursive_mutex
+        m;
+
+    queue< pair<OptCalculation<T>, self*> > ///@todo consider vector
+        queueTodo;
+
+    vector< pair<OptCalculation<T>, self*> >
+        finishedCalculations;
+
+    set<OptBase*> ///@todo rename algorithms, make unique_ptrs
+        pOptimisers;
+
+    bool
+        loggingEnabled;
+
+    bool
+        abortEarly;
+
+    T
+        abortValue;
+
+    ///@todo logging can be done by simply passing a wrapping lambda (write helper for this?)
+    const calc_t<T>
+        calcFunction;
+
+    const OptTarget
+        optTarget;
+
+    T
+        targetValue;
+
+protected: ///@todo these must be stored per-algorithm
+    vector< OptCalculation<T> >
+        previousCalculations;
+
+    OptCalculation<T>
+        bestCalculation;
+
+    const unsigned int
+        maxCalculations; ///@todo can be dropped here / stored globally?
+
+    const OptBoundaries<T>
+        optBoundaries;
+
+//------------------------------------------------------------------------------
+
+public:
+    OptCoordinator(
+        OptBoundaries<T> optBoundaries,
+        unsigned int maxCalculations,
+        calc_t<T> calcFunction,
+        OptTarget optTarget,
+        T targetValue) :
+
+        maxCalculations(maxCalculations),
+        optBoundaries(move(optBoundaries)),
+        calcFunction(move(calcFunction)),
+        optTarget(move(optTarget)),
+        targetValue(move(targetValue))
+    {
+        previousCalculations.reserve(maxCalculations);
+    }
+
+    ///@todo needs method to add child (unique_ptr &&)
+
+//------------------------------------------------------------------------------
+
+    void run_optimisations(unsigned int maxThreads = 1) ///@otod drop the s?
+    {
+        run_optimisations(maxThreads, time(NULL));
+    }
+
+    void run_optimisations(unsigned int maxThreads, unsigned int randomSeed)
+    {
+        srand(randomSeed);
+
+        //get the first to-calculate value of every optimiser
+        //and push it onto the todo queue
+        {
+            auto lck = lock_for();
+            for(const auto &pOptimiser : pOptimisers)
+            {
+                if(pOptimiser->previousCalculations.size() == 0)
+                    push_todo(pOptimiser->get_next_calculation(), pOptimiser);
+            }
+        }
+
+        if constexpr (isMultiThreaded) {
+            vector <thread> threads;
+
+            for(unsigned int i=0; i<maxThreads; ++i)
+                threads.emplace_back(threaded_work);
+
+            for(auto &thread :threads)
+                thread.join();
+        } else
+            threaded_work(); ///@todo rename function
+    }
+
+//------------------------------------------------------------------------------
+
+    void clear_results() ///@todo still offer this?
+    {
+        auto lck = lock_for(); ///@todo might not be required in later flow (see comment below)
+        finishedCalculations.clear();
+    }
+
+//------------------------------------------------------------------------------
+
+    unsigned int number_optimisers()
+    {
+        auto lck = lock_for();
+        return pOptimisers.size(); ///@todo unlikely this has to be locked (only access via the optimisers should be locked)
+    }
+
+//------------------------------------------------------------------------------
+
+    static void enable_early_abort(const T abortVal)
+    {
+        abortEarly = true;
+        abortValue = abortVal;
+    }
+
+//------------------------------------------------------------------------------
+
+    unsigned int number_finished_calculations()
+    {
+        auto lck = lock_for();
+        return finishedCalculations.size();
+    }
+
+//------------------------------------------------------------------------------
+
+    //targetValue won't be used when maximizing or minimizing
+    OptCalculation<T> get_best_calculation(OptTarget optTarget, T targetValue) ///@todo rename to of all, drop params
+    {
+        auto lck = lock_for();
+        OptCalculation<T> out;
+
+        if(finishedCalculations.size() == 0)
+            return out;
+
+        out = finishedCalculations[0].first;
+
+        for(const auto &finishedCalculation : finishedCalculations)
+            if(result_better(finishedCalculation.first, out, optTarget, targetValue))
+                out = finishedCalculation.first;
+
+        return out;
+    }
+
+//------------------------------------------------------------------------------
+
+    OptCalculation<T> get_best_calculation() const
+    {
+       return bestCalculation;
+    }
+
+//------------------------------------------------------------------------------
+
+    unsigned int number_previous_calculations() const
+    {
+        return previousCalculations.size();
+    }
+
+//------------------------------------------------------------------------------
+
+
+protected:
+    //targetValue won't be used when maximizing or minimizing
+    static bool result_better(const OptCalculation<T> &result, const OptCalculation<T> &other, OptTarget optTarget, T targetValue) ///@todo consider implementing this in OptCalculation
+    {
+        switch(optTarget)
+        {
+            case OptTarget::MINIMIZE:
+                return result.result < other.result;
+
+            case OptTarget::MAXIMIZE:
+                return result.result > other.result;
+
+            case OptTarget::APPROACH:
+                return fabs(targetValue - result.result) < fabs(targetValue - other.result);
+
+            case OptTarget::DIVERGE:
+                return fabs(targetValue - result.result) > fabs(targetValue - other.result);
+
+            default: //MINIMIZE
+                return result.result < other.result;
+        }
+    }
+//------------------------------------------------------------------------------
+
+    void add_finished_calculation(OptCalculation<T> optCalculation) ///@todo does too much?
+    {
+        previousCalculations.push_back(optCalculation);
+
+        {
+            auto lck = lock_for();
+            finishedCalculations.push_back({optCalculation, this});
+        }
+
+        if(result_better(optCalculation, bestCalculation, optTarget, targetValue))
+            bestCalculation = optCalculation;
+    }
+
+//------------------------------------------------------------------------------
+
+    unsigned int index_closest_calculation(const vector< OptCalculation<T> > &optCalculations, unsigned int indexThis) const ///@todo could be moved to some helper, also should be easy to simplify heavily
+    {
+        T closestDistance;
+        unsigned int indexClosest(0);
+        bool initialised(false);
+
+        for(unsigned int i = 0; i < optCalculations.size(); ++i)
+        {
+            if(i == indexThis)
+                continue;
+
+            if(!initialised)
+            {
+                closestDistance = optCalculations[i].distance_to(optCalculations[indexThis]);
+                indexClosest = i;
+                initialised = true;
+            }
+            else
+            {
+                T distance = optCalculations[i].distance_to(optCalculations[indexThis]);
+
+                if(distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    indexClosest = i;
+                }
+            }
+        }
+        return indexClosest;
+    }
+
+//------------------------------------------------------------------------------
+
+private:
+    inline auto lock_for() {
+        if constexpr (isMultiThreaded) {
+            return make_optional<lock>(m);
+        } else
+            return nullopt;
+    }
+
+//------------------------------------------------------------------------------
+
+    static void threaded_work() ///@todo rename
+    {
+        while(true)
+        {
+            bool availableTodo(false);
+            pair <OptCalculation<T>, self*> todo;
+            {
+                auto lck = lock_for();
+                availableTodo = available_todo();
+                if(availableTodo)
+                    todo = pop_todo();
+            }
+
+            if(!availableTodo)
+                break;
+
+            OptCalculation<T> optCalculation = todo.first;
+            OptBase* pOptBase = todo.second;
+
+            pOptBase->calcFunction(optCalculation);
+
+            if(loggingEnabled)
+                log(optCalculation);
+
+            pOptBase->add_finished_calculation(optCalculation);
+
+            if(pOptBase->previousCalculations.size() >= pOptBase->maxCalculations)
+                break;
+
+            if(abortEarly)
+            {
+                OptCalculation<T> calcEarly;
+                calcEarly.result = abortValue;
+
+                if(result_better(optCalculation, calcEarly, pOptBase->optTarget , pOptBase->targetValue))
+                    break;
+            }
+
+            //only add the next one if there still are more
+            push_todo(pOptBase->get_next_calculation(), pOptBase);
+        }
+    }
+
+//------------------------------------------------------------------------------
+
+    void push_todo(OptCalculation<T> optCalculation, self *pOptBase) ///@todo all the locking in the functions below could become unnecessary since only the thread callbacks have to use the guards (just lock once there)
+    {
+        auto lck = lock_for();
+        queueTodo.push({optCalculation, pOptBase});
+    }
+
+//------------------------------------------------------------------------------
+
+    void push_finished(OptCalculation<T> optCalculation, self *pOptBase)
+    {
+        auto lck = lock_for();
+        finishedCalculations.push_back({optCalculation, pOptBase});
+    }
+
+//------------------------------------------------------------------------------
+
+    bool available_todo()
+    {
+        auto lck = lock_for();
+        return !queueTodo.empty();
+    }
+
+//------------------------------------------------------------------------------
+
+    pair<OptCalculation<T>, self*> pop_todo()
+    {
+        auto lck = lock_for();
+        auto out = queueTodo.front();
+        queueTodo.pop();
+        return out;
+    }
+
+//------------------------------------------------------------------------------
+
+};
+
+///@todo these can all be removed / initialized within the class since not static anymore
+
+template <typename T, bool isMultiThreaded>
+recursive_mutex OptBase<T, isMultiThreaded>::m;
+
+template <typename T, bool isMultiThreaded>
+queue< pair<OptCalculation<T>, OptBase<T, isMultiThreaded>*> >
+    OptBase<T, isMultiThreaded>::queueTodo;
+
+template <typename T, bool isMultiThreaded>
+vector< pair<OptCalculation<T>, OptBase<T, isMultiThreaded>*> >
+    OptBase<T, isMultiThreaded>::finishedCalculations;
+
+template <typename T, bool isMultiThreaded>
+set<OptBase<T, isMultiThreaded>*>
+    OptBase<T, isMultiThreaded>::pOptimisers;
+
+template <typename T, bool isMultiThreaded>
+bool
+    OptBase<T, isMultiThreaded>::loggingEnabled(false);
+
+template <typename T, bool isMultiThreaded>
+bool
+    OptBase<T, isMultiThreaded>::abortEarly(false);
+
+template <typename T, bool isMultiThreaded>
+T
+    OptBase<T, isMultiThreaded>::abortValue(0);
+
+template <typename T, bool isMultiThreaded>
+string OptBase<T, isMultiThreaded>::loggingDelimiter("");
+
+template <typename T, bool isMultiThreaded>
+string OptBase<T, isMultiThreaded>::loggingLineEnd("");
+
+template <typename T, bool isMultiThreaded>
+ofstream
+    OptBase<T, isMultiThreaded>::logFile;
+
+} // namespace cppOpt
+
+#endif // OPTBASE_H
+
 
 
 
